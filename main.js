1749c21f0f9557bf09c037cbbb0415151d1b5679
EvanRaftery <eraftery@umass.edu>
2024-03-05 13:02:47 -0500

    JSON Implementation
    Python and JS communicating (nearly finalized), GPIO to be implemented soon

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 /*
     ####################
     ##### SDP 2024 #####
@@ -28,171 +27,220 @@
 */
 
 import "@babel/polyfill";
-import * as mobilenetModule from '@tensorflow-models/mobilenet';      // Our CNN
+import * as mobilenetModule from '@tensorflow-models/mobilenet';        // Our CNN
 import * as tf from '@tensorflow/tfjs';
-import * as knnClassifier from '@tensorflow-models/knn-classifier';   // Our classifier
+import * as knnClassifier from '@tensorflow-models/knn-classifier';     // Our Classifier
 
 // Number of classes
-const NUM_CLASSES = 4;
-// Webcam Image size. (Default is 227)
-const IMAGE_SIZE = 500;
-// K value for KNN (number of nearest neighbors to consider)
+const NUM_CLASSES = 5;
+// Webcam Image size (Recommended is 227)
+const IMAGE_SIZE = 227;
+// K value for KNN (Number of nearest neighbors to consider)
 const TOPK = 10;
 // Max delay until object detected
 const MAX_COUNT_DELAY = 50;
 
-// Variables for output logging
+// Variables for object detection
 let prevClass = -1;
 let maxCount = 0;
 
 
 class Main {
-  constructor() {
-    // Initiate variables
-    this.infoTexts = [];
-    this.training = -1; // -1 when no class is being trained
-    this.videoPlaying = false;
+    constructor() {
+        // Initiate variables
+        this.infoTexts = [];
+        this.training = -1;         // -1 when no class is being trained
+        this.videoPlaying = false;
 
-    // Initiate deeplearn.js math and knn classifier objects
-    this.bindPage();
+        // Communication with Python
+        this.pydataT = 0;           // 0 or 1, determines if we're training or not
+        this.pydataC = 0;          // 0 to 5, determines what class we're training
+        this.pydataR = 0;           // 0 or 1, tells us when to reset/retrain
+        this.maxClass = "None";
 
-    // Create video element that will contain the webcam image
-    this.video = document.createElement('video');
-    this.video.setAttribute('autoplay', '');
-    this.video.setAttribute('playsinline', '');
+        // Initiate deeplearn.js math and knn classifier objects
+        this.bindPage();
 
-    // Add video element to DOM
-    //document.body.appendChild(this.video);
+        // Create video element that will contain the webcam image
+        this.video = document.createElement('video');
 
-    // Create training buttons and info texts
-    for (let i = 0; i < NUM_CLASSES; i++) {
-      const div = document.createElement('div');
-      document.body.appendChild(div);
-      div.style.marginBottom = '10px';
-
-      // Create training button
-      //const button = document.createElement('button')
-      //button.innerText = "Train " + i;
-      //div.appendChild(button);
-
-      // Listen for mouse events when clicking the button
-      //button.addEventListener('mousedown', () => this.training = i);
-      //button.addEventListener('mouseup', () => this.training = -1);
-
-      // Listen for key presses for training
-      document.addEventListener('keypress', (event) => {if(event.key === String(i)){this.training = i;}});
-      document.addEventListener('keyup', (event) => {if(event.key === String(i)){this.training = -1;}});
-      
-      // Create info text
-      const infoText = document.createElement('span')
-      infoText.innerText = "No examples added";
-      div.appendChild(infoText);
-      this.infoTexts.push(infoText);
-    }
-
-
-    // Setup webcam
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        this.video.srcObject = stream;
-        this.video.width = IMAGE_SIZE;
-        this.video.height = IMAGE_SIZE;
-
-        this.video.addEventListener('playing', () => this.videoPlaying = true);
-        this.video.addEventListener('paused', () => this.videoPlaying = false);
-      })
-  }
-
-  async bindPage() {
-    this.knn = knnClassifier.create();
-    this.mobilenet = await mobilenetModule.load();
-    
-    this.start();
-  }
-
-  start() {
-    if (this.timer) {
-      this.stop();
-    }
-    this.video.play();
-    this.timer = requestAnimationFrame(this.animate.bind(this));
-  }
-
-  stop() {
-    this.video.pause();
-    cancelAnimationFrame(this.timer);
-  }
-
-  async animate() {
-    if (this.videoPlaying) {
-      // Get image data from the camera
-      const image = tf.fromPixels(this.video);
-
-      let logits;
-      // 'conv_preds' is the logits activation of MobileNet.
-      const infer = () => this.mobilenet.infer(image, 'conv_preds');
-
-      // Train class if one of the buttons/keys is held down
-      if (this.training != -1) {
-        logits = infer();
-
-        // Add current image to classifier
-        this.knn.addExample(logits, this.training)
-      }
-
-      const numClasses = this.knn.getNumClasses();
-
-      // Only run KNN classifier if we have added samples to the model
-      if (numClasses > 0) {
-        // Initialize variables for object detection
-        let max = 0;
-        let maxClass = "None";
-
-        // Run prediction
-        logits = infer();
-        const res = await this.knn.predictClass(logits, TOPK);
-
+        // Show webcam (optional)
+        // document.body.appendChild(this.video);
+        
+        // Create training buttons and info texts    
         for (let i = 0; i < NUM_CLASSES; i++) {
-          // The number of examples for each class
-          const exampleCount = this.knn.getClassExampleCount();
-
-          // Make the predicted class bold
-          if (res.classIndex == i) {
-            this.infoTexts[i].style.fontWeight = 'bold';
-          } else {
-            this.infoTexts[i].style.fontWeight = 'normal';
-          }
-
-          // Update info text
-          if (exampleCount[i] > 0) {
-            this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i] * 100}%`
-
-            // Make note of predictions
-            if (res.confidences[i] > max) {
-              max = res.confidences[i];
-              maxClass = i;
-            }
-          }
+            const div = document.createElement('div');
+            document.body.appendChild(div);
+            div.style.marginBottom = '10px';
+            
+            // Train class N if number key N is pressed
+            document.addEventListener('keypress', (event) => {if(event.key === String(i)){this.training = i;}});
+            document.addEventListener('keyup', (event) => {if(event.key === String(i)){this.training = -1;}});
+            
+            // Create info text
+            const infoText = document.createElement('span')
+            infoText.innerText = " No examples added";
+            div.appendChild(infoText);
+            this.infoTexts.push(infoText);
         }
 
-        // Make note of each prediction and how long that predicted class has been predicted for
-        if (maxClass == prevClass) {    // Same class we've been looking at
-          maxCount = maxCount + 1;
-        } else {                        // New item detected!
-          maxCount = 1;
-          prevClass = maxClass;
-        }
-        console.log(maxClass, maxCount);    // This console log can be accessed by main.py for motor control
-      }
 
-      // Dispose image when done (saves resources)
-      image.dispose();
-      if (logits != null) {
-        logits.dispose();
-      }
+        // Setup webcam
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+            this.video.srcObject = stream;
+            this.video.width = IMAGE_SIZE;
+            this.video.height = IMAGE_SIZE;
+
+            this.video.addEventListener('playing', () => this.videoPlaying = true);
+            this.video.addEventListener('paused', () => this.videoPlaying = false);
+        })
     }
-    this.timer = requestAnimationFrame(this.animate.bind(this));
-  }
+
+    async bindPage() {
+        this.knn = knnClassifier.create();
+        this.mobilenet = await mobilenetModule.load();
+
+        this.start();
+    }
+
+    start() {
+        if (this.timer) {
+            this.stop();
+        }
+        this.video.play();
+        this.timer = requestAnimationFrame(this.animate.bind(this));
+    }
+
+    stop() {
+        this.video.pause();
+        cancelAnimationFrame(this.timer);
+    }
+
+    // Recieve communication from Python
+    async recieve() {
+        let thing = await fetch('http://127.0.0.1:5000/');
+        let things = await thing.json();
+        this.pydataT = things.Train;
+        this.pydataC = things.Clss;
+        this.pydataR = things.Rst;
+        return(things);
+    }
+
+    // Send classifcation info to Python
+    send(clss, c0, c1, c2, c3, c4) {
+        // Account for case where a class has no training data
+        if(!c0){c0 = 0;}
+        if(!c1){c1 = 0;}
+        if(!c2){c2 = 0;}
+        if(!c3){c3 = 0;}
+        if(!c4){c4 = 0;}
+
+        // Set up json data to be sent
+        const data = {
+            i0: c0,
+            i1: c1,
+            i2: c2,
+            i3: c3,
+            i4: c4,
+            class: clss
+        };
+
+        fetch('http://127.0.0.1:5000/', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(responseData => {
+            //console.log('Data sent successfully:', responseData);
+        })
+        .catch(error => {
+            console.error('Error sending data:', error);
+        });
+    }
+
+    logic(){
+        //console.log(this.pydataC);
+        this.training = this.pydataC;
+    }
+
+    async animate() {
+        this.recieve();
+        // this.logic();      // Only going to remove if we do not need anymore logic.
+        this.training = this.pydataC;
+
+        if (this.videoPlaying) {
+            // Get image data from video element
+            const image = tf.fromPixels(this.video);
+
+            let logits;
+            // 'conv_preds' is the logits activation of MobileNet.
+            const infer = () => this.mobilenet.infer(image, 'conv_preds');
+
+            // Train class if one of the buttons is held down
+            if (this.training != -1) {
+                logits = infer();
+
+                // Add current image to classifier
+                this.knn.addExample(logits, this.training)
+            }
+
+            const numClasses = this.knn.getNumClasses();
+            if (numClasses > 0) {
+
+                let max = 0;
+                this.maxClass = "None";
+                let sendClass = 0;
+                // If classes have been added run predict
+                logits = infer();
+                const res = await this.knn.predictClass(logits, TOPK);
+
+                for (let i = 0; i < NUM_CLASSES; i++) {
+                    // The number of examples for each class
+                    const exampleCount = this.knn.getClassExampleCount();
+
+                    // Update info text
+                    if (exampleCount[i] > 0) {
+                        this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i] * 100}%`
+                        if(res.confidences[i] > max) {
+                            max = res.confidences[i];
+                            this.maxClass = i;
+                        }
+                    }
+                }
+                if (this.maxClass == prevClass) {    // Same class we've been looking at
+                    maxCount = maxCount + 1;
+                } else {                        // New item detected!
+                    maxCount = 1;
+                    prevClass = this.maxClass;
+                }
+                /*if (maxCount > MAX_COUNT_DELAY && maxCount % 25 == 0) {
+                this.send(maxClass,this.knn.getClassExampleCount()[0],this.knn.getClassExampleCount()[1], this.knn.getClassExampleCount()[2], this.knn.getClassExampleCount()[3], this.knn.getClassExampleCount()[4]);
+                console.log(maxClass, maxCount); 
+                }else{
+                this.send("None",this.knn.getClassExampleCount()[0],this.knn.getClassExampleCount()[1], this.knn.getClassExampleCount()[2], this.knn.getClassExampleCount()[3], this.knn.getClassExampleCount()[4]);
+                }*/
+            }
+
+            if (maxCount > MAX_COUNT_DELAY && maxCount % 25 == 0) {
+                this.send(this.maxClass,this.knn.getClassExampleCount()[0],this.knn.getClassExampleCount()[1], this.knn.getClassExampleCount()[2], this.knn.getClassExampleCount()[3], this.knn.getClassExampleCount()[4]);
+                console.log(this.maxClass, maxCount); 
+            }else{
+                this.send("None",this.knn.getClassExampleCount()[0],this.knn.getClassExampleCount()[1], this.knn.getClassExampleCount()[2], this.knn.getClassExampleCount()[3], this.knn.getClassExampleCount()[4]);
+            }
+
+            // Dispose image when done
+            image.dispose();
+            if (logits != null) {
+                logits.dispose();
+            }
+        }
+        this.timer = requestAnimationFrame(this.animate.bind(this));
+    }
 }
+
 
 window.addEventListener('load', () => new Main());
--- main.py
+++ main.py
@@ -21,53 +21,89 @@
         Prof. Nikhil Saxena
         
     This is the primary code file for Team 3's SDP. This file revolves around 
-    interfacing with the main.js file in order to receive console output and 
-    relay the decided class to the motor. This file also contains the dedicated 
-    training mode, which allows the user to properly train the model on any 
-    number of items (although the lazy susan only supports 4 bins).
+    interfacing with the main.js file in order to receive output and relay the 
+    decided class to the motor. This file also contains the dedicated training 
+    mode, which allows the user to properly train the model on any number of 
+    items (although the lazy susan only supports 4 bins).
+    
+    The Flask server used to communicate between Python and JS is also set up
+    here, allowing for essential data to be passed to make the program run
+    smoothly.
     
     For more information on the specifics of computer vision and image processing, 
     see the main.js file in this repository.
 """
 
-from selenium import webdriver
-import pyautogui
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
-NUM_CLASSES = 4
+# Set up Flask
+app = Flask(__name__)
+CORS(app)  
 
-def main():
-    # Open Chrome to our local page
-    driver = webdriver.Chrome()
-    driver.get("http://localhost:9966")
-    
-    # Set up console output to be accessed
-    driver.execute_script("""
-    console.stdlog = console.log.bind(console);
-    console.logs = [];
-    console.log = function(){
-        console.logs.push(Array.from(arguments));
-        console.stdlog.apply(console, arguments);
-    }
-    """) 
-    
-    # Training mode, 100 images each class
-    train(100)
-    
-    # Grab the console logs (to be updated with object detection)
-    while True:
-        logs = driver.execute_script("return console.logs")
-        if logs is not None and len(logs) > 0:
-            print(logs[-1])
+global getData
+getData = {
+    "Train": 1,
+    "Clss": -1,
+    "Rst": 0
+}
 
-# Defines the training mode for the model
-def train(duration):
-    # Automatically take photos for each class, in order (duration = samples per class)
-    for classNum in range(NUM_CLASSES):
-        key = str(classNum)
-        for i in range(duration):
-            pyautogui.keyDown(key)
-            pyautogui.keyUp(key)
-        pyautogui.keyUp(key)
+def gpioLogic(data):
+    categories = ["i0", "i1", "i2", "i3", "i4"]
+
+    highestClass = data['class']
+    if highestClass != 'None':
+        print(highestClass)
+        # More logic can go in here
+
+    # GPIO
+    """
+    1. Long button press sets Rst high
+    2. Regular button press for each stage of training
+    3. (Option) Only use one button press and after training mode regular press retrains
+    """
+
+    # Refresh Page
+    """if(data["class"] == 4): # I need to replace this with long button press.(or has js send isRst var)
+        getData["Rst"] = 1
+        #training = 1
+        getData["Train"] = 1
+    else:
+       getData["Rst"] = 0"""
+
+    # Training Mode
+    """
+    Train 0 on just belt
+    Then train each category on and off to get different angles
+    """
+    if(getData["Train"]):
+        if(getData["Clss"] == -1):
+            getData["Clss"] = 0
+        if(getData["Clss"] < 5): 
+            for cat in categories:
+                # The below line should be split into increments
+                # that only goes off when the button flag is high
+                # then sets the button flag low when done.
+                # The button flag can be used for LEDs too.
+                if data[cat] >= 50 and int(cat[1]) == getData["Clss"]:
+                    print(cat, " is over 50: ", data[cat])
+                    getData["Clss"] += 1
+        else:
+            getData["Train"] = 0
+            getData["Clss"] = -1
+
+# Event handling
+@app.route("/", methods=["GET", "POST"])
+@cross_origin()
+def handle_request():
+    if request.method == "POST":
+        data = request.get_json(force=True)
+        gpioLogic(data)
+        return jsonify(data)
+    elif request.method == "GET":
+        return jsonify(getData)  
+    else:
+        return "Method not supported", 405
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
