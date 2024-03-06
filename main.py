66e49f0193bfbbefc7e6823e244c44db6a4158a8
JakeShamaly <jshamaly@umass.edu>
2024-03-05 21:25:06 -0500

    Working Button training.
    Need to implement a mode to do all 50 in 1 go for the plain belt. Maybe get rid of the last class too.

--- main.py
+++ main.py
@@ -40,31 +40,34 @@ import RPi.GPIO as GPIO
 from gpiozero import Button
 import time
 
+
 # Set up Flask
 app = Flask(__name__)
 CORS(app)  
 
+
 # Defining GPIO pins
-BELT = 17
-BIT1 = 27
-BIT2 = 22
+BUTTON_PRESS = 23
+BELT = 26
+BIT1 = 5
+BIT2 = 6
+
 
 # Initialize GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BELT, GPIO.OUT)  # Whether the belt is allowed to move
-GPIO.setup(BIT1, GPIO.OUT)  # Bit 1 of bin
-GPIO.setup(BIT2, GPIO.OUT)  # Bit 2 of bin
-GPIO.setup(23, GPIO.IN)     # Button
+GPIO.setup(BELT, GPIO.OUT)
+GPIO.setup(BIT1, GPIO.OUT)
+GPIO.setup(BIT2, GPIO.OUT)
+GPIO.setup(23, GPIO.IN)
 
-# Initialize Button
+
+
+# Initialize LEDs
 BUTTON = Button(23)
 
-global trainClass
-trainClass = -1
 
-global iter
-iter = 0
 
+# Define data formatting
 global getData
 getData = {
     "Train": 1,
@@ -72,32 +75,41 @@ getData = {
     "Rst": 0
 }
 
+global trainClass
+trainClass = -1
+
+global itera
+itera = 0
+
+
 def gpioLogic(data):
+    global trainClass
+    global itera
+    categories = ["i0", "i1", "i2", "i3", "i4"]
     if BUTTON.is_pressed:
-        if getData["Train"]:
-            iter += 1
+        if getData["Train"] and not (itera*10 > data[categories[trainClass]]):
+            itera += 1
+            time.sleep(1)
         else:
             getData["Train"] = 1
-    categories = ["i0", "i1", "i2", "i3", "i4"]
+            
+
 
     highestClass = data['class']
-    if highestClass > 0 and trainClass == -1:
-        # print(highestClass)
-        
+    if highestClass > 0:
+        print(highestClass)
+       
         if highestClass >= 2:
-            GPIO.output(BIT1, 1)
+            GPIO.output(BIT1 , 1)
         else:
-            GPIO.output(BIT1, 0)
-        
+            GPIO.output(BIT1 , 0)
+           
         if highestClass % 2 == 0:
-            GPIO.output(BIT2, 0)
+            GPIO.output(BIT2 , 0)
         else:
-            GPIO.output(BIT2, 1)
-        time.sleep(2)
-        
-        GPIO.output(BELT, 1)
-        time.sleep(2)
-        GPIO.output(BELT, 0)
+            GPIO.output(BIT2 , 1)
+        # Use highestClass to move shit
+
 
     # GPIO
     """
@@ -105,6 +117,7 @@ def gpioLogic(data):
     2. Regular button press for each stage of training
     3. (Option) Only use one button press and after training mode regular press retrains
     """
+
 
     # Refresh Page
     """if(data["class"] == 4): # I need to replace this with long button press.(or has js send isRst var)
@@ -114,65 +127,32 @@ def gpioLogic(data):
     else:
        getData["Rst"] = 0"""
 
+
     # Training Mode
     """
     Train 0 on just belt
     Then train each category on and off to get different angles
     """
-
-    # Below is the working code
-
-    """
     if(getData["Train"]):
-        if(getData["Clss"] == -1):
-            getData["Clss"] = 0
-        if(getData["Clss"] < 5):
-            for cat in categories:
-                # The below line should be split into increments
-                # that only goes off when the button flag is high
-                # then sets the button flag low when done.
-                # The button flag can be used for LEDs too.
-                if data[cat] >= 50 and int(cat[1]) == getData["Clss"]:
-                    print(cat, " is over 50: ", data[cat])
-                    getData["Clss"] += 1
-        else:
-            getData["Train"] = 0
-            getData["Clss"] = -1
-    """
-
-        # trainClass represents the class currently being trained
-    
-    # getData["Clss"] tells the JS when and what to train 
-    # (-1 when waiting for button press so trainClass stores 
-    # value for when next iteration of training starts)
-
-    # getData["Train"] is a flag for Python to know when to retrain
-
-    # iter is used for the training iterations
-
-    if(getData["Train"]):
-
+        print(itera)
         if(trainClass == -1):
             trainClass = 0
-        if(trainClass < 5): 
+        if(trainClass < 5):
             for cat in categories:
                 if(int(cat[1]) == trainClass):
-                    if data[cat] >= (iter*10):
+                    if(data[cat] >= (itera*10)):
                         getData["Clss"] = -1
                     else:
-                       getData["Clss"] = trainClass
-
-                # Might need to move this or change conditional
+                        getData["Clss"] = trainClass
                 if data[cat] >= 50 and int(cat[1]) == getData["Clss"]:
                     print(cat, " is over 50: ", data[cat])
                     trainClass += 1
-                    iter = 0
+                    itera = 0
                     #getData["Clss"] += 1
         else:
-           getData["Train"] = 0
-           trainClass = -1
-           getData["Clss"] = -1
-
+            getData["Train"] = 0
+            trainClass  = -1
+            getData["Clss"] = -1
 
 
 # Event handling
@@ -187,6 +167,7 @@ def handle_request():
         return jsonify(getData)  
     else:
         return "Method not supported", 405
+
 
 if __name__ == "__main__":
     app.run(debug=True)
