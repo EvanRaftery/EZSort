d9428047d6a723f0c589570995db800ea2fb12fa
JakeShamaly <jshamaly@umass.edu>
2024-03-07 11:29:45 -0500

    All but actual pin connections code DONE for NanoB
    No use of yellow light right now

--- NanoB_CDR/NanoB_CDR.ino
+++ NanoB_CDR/NanoB_CDR.ino
@@ -1,7 +1,7 @@
-const int onOff = 6;
-const int dir = 7;
-const int onOffi = 8;
-const int diri = 9;
+const int onOff = 7;
+const int dir = 6;
+const int onOffi = A0; //8;
+int diri = 1;
 const int high = 10;
 
 const int blink1 = 3;
@@ -14,22 +14,24 @@ void setup() {
   pinMode(onOff, OUTPUT);
   pinMode(dir, OUTPUT);
   pinMode(onOffi, INPUT);
-  pinMode(diri, INPUT);
+  //pinMode(diri, INPUT); No longer needed, one direction so I will hard code it for now.
   pinMode(blink1, OUTPUT);
   pinMode(blink2, OUTPUT);
   pinMode(blink3, OUTPUT);
+  diri = 1; // Set low if belt goes wrong way
+  
   
 }
 
 void loop() {
   //digitalWrite(fw, HIGH);
-  if(digitalRead(diri) == HIGH && digitalRead(onOffi) == HIGH){
+  if(diri == HIGH && digitalRead(onOffi) == HIGH){//if(digitalRead(diri) == HIGH && digitalRead(onOffi) == HIGH){
     digitalWrite(dir, HIGH);
     digitalWrite(blink1, HIGH);
     digitalWrite(blink2, LOW);
     digitalWrite(blink3, LOW);
     }
-  else if(digitalRead(diri) == LOW && digitalRead(onOffi) == HIGH){
+  else if(diri == LOW && digitalRead(onOffi) == HIGH){//else if(digitalRead(diri) == LOW && digitalRead(onOffi) == HIGH){
     digitalWrite(dir, LOW);
     digitalWrite(blink2, HIGH);
     digitalWrite(blink1, LOW);
